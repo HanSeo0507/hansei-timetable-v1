b5985dd2fbe87684444b79dc18ea80f1bb022175
HanSeo0507 <khan3618@naver.com>
2021-03-09 22:55:10 +0900

    Add: react-cookie Provider & AuthContext Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { CookiesProvider } from "react-cookie";
+
 import Router from "src/routes";
+import AuthProvider from "src/Auth";
+import { GlobalStyled } from "src/components";
 
 const App: React.FC = () => {
 	return (
-		<>
-			<Router />
-		</>
+		<CookiesProvider>
+			<GlobalStyled />
+			<AuthProvider>
+				<Router />
+			</AuthProvider>
+		</CookiesProvider>
 	);
 };
 
