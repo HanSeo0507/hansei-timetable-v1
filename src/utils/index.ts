043bf6c0613f0b899b15fcf39c11d0768cfa8999
HanSeo0507 <khan3618@naver.com>
2021-03-10 02:13:48 +0900

    Add: getNowSubject, getNextSubject, getLefttime, formatTime, fillZero functions
    getNowSubject - 현재 수업 과목을 가져옵니다.
    
    getNextSubject - 다음 수업 과목의 index와 과목명을 가져옵니다.
    
    getLefttime - 입력된 두 시간 값을 통해 계산하고 남은 시간을 알려줍니다.
    
    formatTime - 입력된 시간을 포맷팅하여 다시 리턴합니다.
    
    fillZero - 10 이하의 숫자 앞에 0을 붙입니다 (9 -> 09)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,4 +7,71 @@ const validatorUser = (user: any) => {
 	return false;
 };
 
-export { validatorUser };
+const getDay = (day: number) => {
+	if (day === 0) return 6;
+	return day - 1;
+};
+
+type IClassTime = {
+	start: number;
+	finish: number;
+};
+
+type INextClassTime = {
+	start: number;
+	finish: number;
+	next: number;
+	type?: string;
+};
+
+const getNowSubject = (date: Date, classTime: IClassTime[]) => {
+	const nowClass = classTime.find((v, i) => new Date(v.start).getTime() <= date.getTime() && date.getTime() <= new Date(v.finish).getTime());
+
+	if (nowClass) {
+		let index = classTime.indexOf(nowClass);
+		return index;
+	}
+
+	return -1;
+};
+
+const getNextSubject = (date: Date, classTime: INextClassTime[], timetable: string[]): { index: number; class: string; type: string } | undefined => {
+	const nextClass = classTime.find((v, i) => new Date(v.start).getTime() <= date.getTime() && date.getTime() <= new Date(v.finish).getTime());
+
+	if (nextClass) {
+		return { index: nextClass.next, class: timetable[nextClass.next], type: nextClass.type === "launch" ? "점심시간" : "쉬는시간" };
+	}
+
+	return undefined;
+};
+
+const getLeftTime = (fisrtDate: number, secondDate: number): string => {
+	const leftTime = fisrtDate - secondDate;
+
+	const hours = Math.floor((leftTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const miniutes = Math.floor((leftTime % (1000 * 60 * 60)) / (1000 * 60));
+
+	const message = hours ? `${hours}시간 ${miniutes}분` : `${miniutes}분`;
+
+	return message;
+};
+
+const formatTime = (now: Date) => {
+	const month = now.getMonth() + 1;
+	const date = now.getDate();
+	const hour = now.getHours();
+	const min = now.getMinutes();
+	const sec = now.getSeconds();
+
+	const isAM = hour < 12;
+
+	return `${month}월 ${date}일 ${isAM ? "오전" : "오후"} ${hour}시 ${fillZero(min)}분 ${sec}초`;
+};
+
+const fillZero = (number: number) => {
+	if (number < 10) return `0${number.toString()}`;
+
+	return number;
+};
+
+export { validatorUser, getDay, getNowSubject, getNextSubject, getLeftTime, formatTime };
